4f7b97b9b2c452398250942405e640c18358dc50
zx64 <zx64@users.sf.net>
2007-01-03 22:09:26 +0000

    Remove another include and reformat path.h slightly

--- src/freecnc/game/path.h
+++ src/freecnc/game/path.h
@@ -2,16 +2,15 @@
 #define _GAME_PATH_H
 
 #include <stack>
-#include "../freecnc.h"
 
 class Path
 {
 public:
     Path(unsigned int crBeg, unsigned int crEnd, unsigned char max_dist);
 
-    bool empty() const {return result.empty();}
-    unsigned char top() const {return result.top();}
-    void pop() {result.pop();}
+    bool empty() const { return result.empty(); }
+    unsigned char top() const { return result.top(); }
+    void pop() { result.pop(); }
 private:
     std::stack<unsigned char> result;
 };
