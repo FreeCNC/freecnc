e534fb6c353ca29b49e77b4c834b541cac6b632c
zx64 <zx64@users.sf.net>
2007-01-09 22:07:35 +0000

    Clean up IMADecode a bit and fix sound quality issue.

--- src/freecnc/sound/soundfile.cpp
+++ src/freecnc/sound/soundfile.cpp
@@ -9,16 +9,19 @@
 
 namespace Sound
 {
-    const int Steps[89] = {
-        7,8,9,10,11,12,13,14,16,17,19,21,23,25,28,31,
-        34,37,41,45,50,55,60,66,73,80,88,97,107,118,130,143,
-        157,173,190,209,230,253,279,307,337,371,408,449,494,544,598,658,
-        724,796,876,963,1060,1166,1282,1411,1552,1707,1878,2066,2272,2499,
-        2749,3024,3327,3660,4026,4428,4871,5358,5894,6484,7132,7845,8630,
-        9493,10442,11487,12635,13899,15289,16818,18500,20350,22385,24623,
-        27086,29794,32767
+    const int steps[89] = {
+        7,     8,     9,     10,    11,    12,     13,    14,    16,
+        17,    19,    21,    23,    25,    28,     31,    34,    37,
+        41,    45,    50,    55,    60,    66,     73,    80,    88,
+        97,    107,   118,   130,   143,   157,    173,   190,   209,
+        230,   253,   279,   307,   337,   371,    408,   449,   494,
+        544,   598,   658,   724,   796,   876,    963,   1060,  1166,
+        1282,  1411,  1552,  1707,  1878,  2066,   2272,  2499,  2749,
+        3024,  3327,  3660,  4026,  4428,  4871,   5358,  5894,  6484,
+        7132,  7845,  8630,  9493,  10442, 11487,  12635, 13899, 15289,
+        16818, 18500, 20350, 22385, 24623, 27086,  29794, 32767
     };
-    const int Indexes[8] = {-1,-1,-1,-1,2,4,6,8};
+    const int index_adjustment[8] = {-1,-1,-1,-1,2,4,6,8};
 
     // Decode Westwood's ADPCM format.  Original code from ws-aud.txt by Asatur V. Nazarian
     #define HIBYTE(word) ((word) >> 8)
@@ -49,53 +52,43 @@ namespace Sound
 
     void IMADecode(unsigned char *output, unsigned char *input, unsigned short compressed_size, int& sample, int& index)
     {
-        int  Samples;
-        unsigned char Code;
-        int  Sign;
-        int  Delta;
-        unsigned char *InP;
-        short *OutP;
-        unsigned short uncompressed_size = compressed_size * 2;
-
         if (compressed_size==0)
             return;
 
-        InP=(unsigned char *)input;
-        OutP=(short *)output;
+        unsigned short uncompressed_size = compressed_size * 2;
 
-        for (Samples=0; Samples<uncompressed_size; Samples++) {
-            if (Samples&1)          // If Samples is odd
-                Code=(*InP++)>>4;    // Extract upper 4 bits
-            else                         // Samples is even
-                Code=(*InP) & 0x0F;  // Extract lower 4 bits
+        short* formatted_output = reinterpret_cast<short*>(output);
 
-            Sign=(Code & 0x08)!=0;  // If topmost bit is set, Sign=true
-            Code&=0x07;             // Keep lower 3 bits
+        // Each byte is processed twice: lower then upper half
+        for (int samples = 0; samples < uncompressed_size; ++samples) {
+            unsigned char code;
+            if (samples & 1) {
+                code = (*input++) >> 4;
+            } else {
+                code = (*input) & 0xF;
+            }
 
-            Delta=0;
-            if ((Code & 0x04)!=0)
-                Delta+=Steps[index];
-            if ((Code & 0x02)!=0)
-                Delta+=Steps[index]>>1;
-            if ((Code & 0x01)!=0)
-                Delta+=Steps[index]>>2;
-            Delta+=Steps[index]>>3;
+            int step = steps[index];
+            int delta = step >> 3;
 
-            if (Sign)
-                Delta=-Delta;
+            if (code & 1) {
+                delta += step >> 2;
+            }
+            if (code & 2) {
+                delta += step >> 1;
+            }
+            if (code & 4) {
+                delta += step;
+            }
+            if (code & 8) {
+                sample -= delta;
+            } else {
+                sample += delta;
+            }
 
-            sample+=Delta;
+            *formatted_output++ = Clip<short>(sample);
 
-            // When sample is too high it should wrap
-            if (sample > 0x7FFF)
-                sample = Delta;
-            // TODO I'm not happy with this, if we do sample = 0 or sample -= 0x7FFF we get
-            // popping noises. sample = Delta seems to have the smallest pop
-            // If we do nothing, then audio after X seconds gets stuck
-
-            *OutP++ = Clip<short>(sample);
-
-            index+=Indexes[Code];
+            index += index_adjustment[code & 7];
             index = Clip<unsigned char>(index, 0, 88);
         }
     }
