79bde37cb5eb98ce657ceace306e474b6bc8dbae
zx64 <zx64@users.sf.net>
2007-06-21 21:38:30 +0000

    Make td's manifest a bit more neat

--- data/td/manifest.lua
+++ data/td/manifest.lua
@@ -1,12 +1,19 @@
-vfs_add {"./", required="all"}
-vfs_add {"local.mix",   "cclocal.mix", required="any"}
+vfs_add {"./"}
 vfs_add {"conquer.mix", "general.mix", required="all"}
-vfs_add {"general-gdi.mix", "general-nod.mix", required="none"}
-vfs_add {"movies.mix",  "movies-gdi.mix", "movies-nod.mix", required="none"}
-vfs_add {"sc-000.mix",  "sc-001.mix", required="none"}
-vfs_add {"scores.mix", "setup.mix", required="none"}
 vfs_add {"aud.mix", "speech.mix", "sounds.mix", required="all"}
 vfs_add {"transit.mix", required="all"}
-vfs_add {"update.mix",   "updatec.mix", required="any"}
 vfs_add {"desert.mix",   "temperat.mix", "winter.mix", required="all"}
-vfs_add {"deseicnh.mix", "tempicnh.mix", "winticnh.mix", required="any"}
+vfs_add {"movies.mix", "scores.mix", required="none"}
+-- local.mix in DOS version, cclocal.mix later versions
+vfs_add {"local.mix",   "cclocal.mix", required="any"}
+
+-- FreeCNC convention for working with GDI and NOD CDs at the same time
+vfs_add {"general-gdi.mix", "general-nod.mix", required="none"}
+vfs_add {"movies-gdi.mix", "movies-nod.mix", required="none"}
+
+-- Gold Edition specific files
+vfs_add {"update.mix",   "updatec.mix", required="none"}
+vfs_add {"deseicnh.mix", "tempicnh.mix", "winticnh.mix", required="none"}
+
+-- Covert Operations files
+vfs_add {"sc-000.mix",  "sc-001.mix", required="none"}
