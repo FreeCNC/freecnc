a01ece31b8a02eddab50a4597fdde815950ed50c
zx64 <zx64@users.sf.net>
2007-06-22 04:35:10 +0000

    Move general.mix into an any group with general-gdi.mix and general-nod.mix

--- data/td/manifest.lua
+++ data/td/manifest.lua
@@ -1,15 +1,13 @@
 vfs_add {"./"}
-vfs_add {"conquer.mix", "general.mix", required="all"}
+vfs_add {"conquer.mix", required="all"}
 vfs_add {"aud.mix", "speech.mix", "sounds.mix", required="all"}
 vfs_add {"transit.mix", required="all"}
 vfs_add {"desert.mix",   "temperat.mix", "winter.mix", required="all"}
-vfs_add {"movies.mix", "scores.mix", required="none"}
+vfs_add {"scores.mix", required="none"}
 -- local.mix in DOS version, cclocal.mix later versions
 vfs_add {"local.mix",   "cclocal.mix", required="any"}
-
--- FreeCNC convention for working with GDI and NOD CDs at the same time
-vfs_add {"general-gdi.mix", "general-nod.mix", required="none"}
-vfs_add {"movies-gdi.mix", "movies-nod.mix", required="none"}
+vfs_add {"general.mix", "general-gdi.mix", "general-nod.mix", required="any"}
+vfs_add {"movies.mix", "movies-gdi.mix", "movies-nod.mix", required="none"}
 
 -- Gold Edition specific files
 vfs_add {"update.mix",   "updatec.mix", required="none"}
